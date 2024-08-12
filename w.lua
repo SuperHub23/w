ca54cc0796cf9ddeb3f22fedd389f1f759a9745d
SuperHub23 <168860458+SuperHub23@users.noreply.github.com>
2024-08-12 19:46:36 +0200

    Update w.lua

--- w.lua
+++ w.lua
@@ -1,11 +1,4 @@
--- Define a Lua table
-local myTable = {
-  "00000000000000000000001a7dda7110",
-  "987654321"
+return {
+    "7195477259",
+    "987654321"
 }
-end
-
--- Print all elements in the table
-for i, value in ipairs(myTable) do
-  print("Element " .. i .. ": " .. value)
-end
